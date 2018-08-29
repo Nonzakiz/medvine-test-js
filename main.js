58be6303d034c2b72b1026fc6950044e5e26eb77
Tawan Rungsang <s5601081616453@email.kmutnb.ac.th>
2018-08-29 16:10:32 +0700

    Update tacking

--- main.js
+++ main.js
@@ -1,14 +1,63 @@
 const axios = require('axios')
-
+const cheerio = require('cheerio')
 const html = axios
-    .get('https://github.com/Nonzakiz')
-    .then(result => result)
-const html3 = axios
-    .get('https://github.com/Nonzakiz')
-    .then(result => result)
-const html2 = axios
-    .get('https://github.com/Nonzakiz')
-    .then(result => result)
+    .get('http://thagnexs.cjkx.net/web/g_detail.jsp?slipno=600013857115')
+    .then(result => result.data)
+
 Promise
-    .all([html,html2,html3])
-    .then(result => console.log(result))
+    .all([html])
+    .then(result => {
+        /*Bill No*/
+        const $ = cheerio.load(result[0])
+        const Nobill = $('table').eq(0).text().trim().replace('Bill No. :','').split('(')
+        const tacking = Nobill[0].trim()
+        const status = Nobill[1].trim().replace(')','')
+
+        /*Basic Info*/
+        const Sender = $('table').eq(2).find('tr').eq(1).find('td').eq(0).text().trim()
+        const Recipient = $('table').eq(2).find('tr').eq(1).find('td').eq(1).text().trim()
+        const Qty = $('table').eq(2).find('tr').eq(1).find('td').eq(2).text().trim()
+        const Consignee = $('table').eq(2).find('tr').eq(1).find('td').eq(3).text().trim()
+
+        /*Agent Info & DM Info */
+        const Division = $('table').eq(4).find('tr').eq(1).find('td').eq(0).text().trim()
+        const AgentName = $('table').eq(4).find('tr').eq(1).find('td').eq(1).text().trim()
+        const AgentTel = $('table').eq(4).find('tr').eq(1).find('td').eq(2).text().trim()
+        const DM = $('table').eq(4).find('tr').eq(1).find('td').eq(3).text().trim()
+        const DMMobile = $('table').eq(4).find('tr').eq(1).find('td').eq(4).text().trim()
+
+        const Division1 = $('table').eq(4).find('tr').eq(2).find('td').eq(0).text().trim()
+        const AgentName1 = $('table').eq(4).find('tr').eq(2).find('td').eq(1).text().trim()
+        const AgentTel1 = $('table').eq(4).find('tr').eq(2).find('td').eq(2).text().trim()
+        const DM1 = $('table').eq(4).find('tr').eq(2).find('td').eq(3).text().trim()
+        const DMMobile1 = $('table').eq(4).find('tr').eq(2).find('td').eq(4).text().trim()
+
+        const output = { "tracking_id":tacking,
+        "sender": Sender,
+        "recipient": Recipient,
+        "consignee": Consignee,
+        "agent": {
+            "pickup": {
+                "agent_name": AgentName,
+                "agent_tel": AgentTel,
+                "dm": DM,
+                "dm_phone": DMMobile,
+            },
+            "delivery": {
+                "agent_name": AgentName1,
+                "agent_tel": AgentTel1,
+                "dm": DM1,
+                "dm_phone": DMMobile1,
+              }
+        },
+        }
+        console.log(output)
+    
+
+ 
+
+
+    
+
+
+    })
